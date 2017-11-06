d40b1bc52d5d9cb44e123457587cf5bb6768b36d
weiyoudi <742650941@qq.com>
2017-11-06 19:39:18 +0800

    StringUtils.isEmpty(user.getCompanyId())

--- src/main/java/com/xiangshangban/transit_service/controller/RedirectController.java
+++ src/main/java/com/xiangshangban/transit_service/controller/RedirectController.java
@@ -39,16 +39,7 @@ public class RedirectController{
 	 */
 	@RequestMapping("/sendRequest")
     public String register(HttpServletRequest request) {
-		String uri = request.getParameter("url");//转发路径
-		String modeCode = request.getParameter("mode");//模块
-		uri = "attendance/test/countDaily";
-		String sendurl = HttpClientUtil.getModeUrl(modeCode)+uri;
-		Map<String,String[]> paramMap =  (Map<String,String[]>)request.getParameterMap();
-		JSONObject newParamMap = new JSONObject();
-		for (Map.Entry<String,String[]> entry : paramMap.entrySet()) {
-			logger.info(entry.getKey()+":"+entry.getValue()[0]);
-			newParamMap.put(entry.getKey(), entry.getValue().length>1?entry.getValue():entry.getValue()[0]);
-		}
+		
 		//根据token获得当前用户id,公司id
 		String token = request.getHeader("ACCESS_TOKEN");
 		if(StringUtils.isEmpty(token)){
@@ -58,16 +49,35 @@ public class RedirectController{
 				token = login.getToken();
 			}else{
 				ReturnData returnData = new ReturnData();
-				returnData.setReturnCode("3001");
+				returnData.setReturnCode("3003");
 				returnData.setMessage("用户身份获取失败");
 				return JSON.toJSONString(returnData);
 			}
 		}
 		Uusers user = userService.selectCompanyByToken(token);
+		if(StringUtils.isEmpty(user.getCompanyId()) || StringUtils.isEmpty(user.getUserid())){
+			ReturnData returnData = new ReturnData();
+			returnData.setReturnCode("3003");
+			returnData.setMessage("用户身份获取失败");
+			return JSON.toJSONString(returnData);
+		}
+		
+		String uri = request.getParameter("redirectUrl");//转发路径
+		String modeCode = request.getParameter("redirectMode");//模块
+		String sendurl = HttpClientUtil.getModeUrl(modeCode)+uri;
+		//头信息
 		Map<String,String> headers = new HashMap<String,String>();
 		headers.put("companyId", user.getCompanyId());
 		headers.put("accessUserId", user.getUserid());
-		
+		//请求参数
+		Map<String,String[]> paramMap =  (Map<String,String[]>)request.getParameterMap();
+		JSONObject newParamMap = new JSONObject();
+		for (Map.Entry<String,String[]> entry : paramMap.entrySet()) {
+			logger.info(entry.getKey()+":"+entry.getValue()[0]);
+			if(!entry.getKey().equals("redirectUrl") && !entry.getKey().equals("redirectMode")){
+				newParamMap.put(entry.getKey(), entry.getValue().length>1?entry.getValue():entry.getValue()[0]);
+			}
+		}
 		//ContentType contentType = ContentType.create(req.getHeader("content-type").split(";")[0], "UTF-8");
 		String result = HttpClientUtil.sendRequet(sendurl, newParamMap, ContentType.APPLICATION_JSON, headers);//ContentType.APPLICATION_JSON);
         return result;
