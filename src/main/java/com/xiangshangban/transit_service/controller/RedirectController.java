db438bfd4133b62e70b07d5bcf5c9c3c2786ad29
weiyoudi <742650941@qq.com>
2017-11-06 17:12:13 +0800

    再次修改完善转发

--- src/main/java/com/xiangshangban/transit_service/controller/RedirectController.java
+++ src/main/java/com/xiangshangban/transit_service/controller/RedirectController.java
@@ -39,17 +39,7 @@ public class RedirectController{
 	 */
 	@RequestMapping("/sendRequest")
     public String register(HttpServletRequest request) {
-		String uri = request.getParameter("redirectUrl");//转发路径
-		String modeCode = request.getParameter("redirectMode");//模块
-		String sendurl = HttpClientUtil.getModeUrl(modeCode)+uri;
-		Map<String,String[]> paramMap =  (Map<String,String[]>)request.getParameterMap();
-		JSONObject newParamMap = new JSONObject();
-		for (Map.Entry<String,String[]> entry : paramMap.entrySet()) {
-			logger.info(entry.getKey()+":"+entry.getValue()[0]);
-			if(!entry.getKey().equals("redirectUrl") && !entry.getKey().equals("redirectMode")){
-				newParamMap.put(entry.getKey(), entry.getValue().length>1?entry.getValue():entry.getValue()[0]);
-			}
-		}
+		
 		//根据token获得当前用户id,公司id
 		String token = request.getHeader("ACCESS_TOKEN");
 		if(StringUtils.isEmpty(token)){
@@ -71,10 +61,25 @@ public class RedirectController{
 			returnData.setMessage("用户身份信息缺失");
 			return JSON.toJSONString(returnData);
 		}
+		
+		
+		//准备调用子模块接口
+		String uri = request.getParameter("redirectUrl");//转发路径
+		String modeCode = request.getParameter("redirectMode");//模块
+		String sendurl = HttpClientUtil.getModeUrl(modeCode)+uri;
+		//请求头
 		Map<String,String> headers = new HashMap<String,String>();
 		headers.put("companyId", user.getCompanyId());
 		headers.put("accessUserId", user.getUserid());
-		
+		//请求参数（放在body中）
+		Map<String,String[]> paramMap =  (Map<String,String[]>)request.getParameterMap();
+		JSONObject newParamMap = new JSONObject();
+		for (Map.Entry<String,String[]> entry : paramMap.entrySet()) {
+			logger.info(entry.getKey()+":"+entry.getValue()[0]);
+			if(!entry.getKey().equals("redirectUrl") && !entry.getKey().equals("redirectMode")){
+				newParamMap.put(entry.getKey(), entry.getValue().length>1?entry.getValue():entry.getValue()[0]);
+			}
+		}
 		//ContentType contentType = ContentType.create(req.getHeader("content-type").split(";")[0], "UTF-8");
 		String result = HttpClientUtil.sendRequet(sendurl, newParamMap, ContentType.APPLICATION_JSON, headers);//ContentType.APPLICATION_JSON);
         return result;
